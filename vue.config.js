e21c432fae6ffdcba4a0319d06ecb2f43bf2c1ff
johnny <qlu@knx.com.cn>
2022-11-01 00:02:02 +0800

    feature: project-init

---
+++ vue.config.js
@@ -0,0 +1,12 @@
+const path = require('path')
+const WindiCSSWebpackPlugin = require('windicss-webpack-plugin')
+const MiniProgramTailwindWebpackPlugin = require("@dcasia/mini-program-tailwind-webpack-plugin")
+
+module.exports = {
+	configureWebpack: {
+		plugins: [
+			new WindiCSSWebpackPlugin(),
+			new MiniProgramTailwindWebpackPlugin({enableRpx: false}),
+		]
+	}
+}
